52d52b7272815ea750c49684212a492061337c6d
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-08-24 15:38:48 +0100

    Create Cell Class()
    Contains a 'visited' property.

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// make cell object / class
+class Cell {
+    constructor() {
+        this.visited = false 
+    }
+}
+
+// create knight object
+// make gameboard with 8x8 cells
+let gameBoard = 
+function knightMoves() {
+
+}
