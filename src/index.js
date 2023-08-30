6d90f6630f53cf470d8aef8ae90fa5bff1f94c28
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-08-30 13:57:25 +0100

    Introduce PartOfChain Property To Node Objects
    When the final position is reached, the previous position's node will
    have its partOfChain value set to true.
    For each position part of the queue, test if it has a neighbouring move
    that is part of the chain. If so, make it part of the chain, and return
    the node + the chain. If not, just return the chain.

--- src/index.js
+++ src/index.js
@@ -12,198 +12,113 @@ function isArrayInArray(array, item) {
     return result != null;
 }
 
-// required to construct tree for recursive traversal
-
-// let's try to replace the coordinates with nodes
 class Node {
     constructor(col, row) {
         this.coords = [col, row]
-        this.nextMoves = []
+        this.neighbouringMoves = []
+        this.partOfChain = false;
+        if (col+2 < 8 && row+1 < 8) this.neighbouringMoves.push([col+2, row+1])
+        if (col+1 < 8 && row+2 < 8) this.neighbouringMoves.push([col+1, row+2])
+        if (col-1 >=0 && row+2 < 8) this.neighbouringMoves.push([col-1, row+2])
+        if (col-2 >=0 && row+1 < 8) this.neighbouringMoves.push([col-2, row+1])
+        if (col-2 >=0 && row-1 >=0) this.neighbouringMoves.push([col-2, row-1])
+        if (col-1 >=0 && row-2 >=0) this.neighbouringMoves.push([col-1, row-2])
+        if (col+1 < 8 && row-2 >=0) this.neighbouringMoves.push([col+1, row-2])
+        if (col+2 < 8 && row-1 >=0) this.neighbouringMoves.push([col+2, row-1])
+
     }
 }
 
-// contains each position and their moves
-class Graph {
-    constructor(numVertices) {
-        this.numVertices = numVertices;
-        this.AdjList = new Map();
-    }
-
-    addVertex(v) {
-        this.AdjList.set(v, [])
-    }
-
-    getVertex(col, row) {
-        for (let [vertex, movesList] of this.AdjList) {
-            if (col === vertex[0] && row === vertex[1]) {
-                // return key
-                return [vertex, movesList]
-            }
-        }
-    }
-
-    addEdge(v, w) {
-        this.AdjList.get(v).push(w);
-    }
-
-    printGraph() {
-        let getKeys = this.AdjList.keys();
-        for (let i of getKeys) {
-            let getValues = this.AdjList.get(i)
-            let conc = "";
-        }
-
-        for (let j of getValues) conc += j + " ";
-        console.log(i + " -> " + conc);
-    }
-}
-
-let newGraph = new Graph(64);
 let vertices = []
 let sideLength = 8
 // this populates the vertices array with coordinates of the chessboard
 for (let i = 0; i < sideLength; i++) {
     for (let j = 0; j < sideLength; j++) {
         let newNode = new Node(i, j)
-        // let coords = [i, j]
-        // vertices.push(coords)
         vertices.push(newNode)
     }
 }
 
-// let's not use Graph... 
-// add all these vertices, to inside the object
-// for (v of vertices) {
-//     newGraph.addVertex(v);
-// }
-
-// try to add the (up to ) 8 possible moves for each vertex
-// for each of the 8 moves, test if out of bounds
-// if not out of bounds, get the resulting vertex of the move, then add it to the moves list
-for (v of vertices) {
-
-    // urr
-    // if (v[0]+2 < 8 && v[1]+1 < 8) {
-    if (v.coords[0]+2 < 8 && v.coords[1]+1 < 8) {
-        // let move = newGraph.getVertex(v[0]+2,v[1]+1)[0]
-        v.nextMoves.push(v.coords[0]+2, v.coords[1]+1)
-        // newGraph.addEdge(v, move)
-    } 
-
-    //uur
-    // if (v[0]+1 < 8 && v[1]+2 < 8) {
-    if (v.coords[0]+1 < 8 && v.coords[1]+2 < 8) {
-        // let move = newGraph.getVertex(v[0]+1,v[1]+2)[0]
-        // newGraph.addEdge(v, move)
-        v.nextMoves.push(v.coords[0]+1, v.coords[1]+2)
-    } 
-
-    //uul
-    // if (v[0]-1 >=0 && v[1]+2 < 8) {
-    if (v.coords[0]-1 >=0 && v.coords[1]+2 < 8) {
-        // let move = newGraph.getVertex(v[0]-1,v[1]+2)[0]
-        // newGraph.addEdge(v, move)
-        v.nextMoves.push(v.coords[0]-1, v.coords[1]+2)
-    } 
-
-    //ull
-    // if (v[0]-2 >=0 && v[1]+1 < 8) {
-    if (v.coords[0]-2 >=0 && v.coords[1]+1 < 8) {
-        // let move = newGraph.getVertex(v[0]-2,v[1]+1)[0]
-        // newGraph.addEdge(v, move)
-        v.nextMoves.push(v.coords[0]-2, v.coords[1]+1)
-    } 
-
-    //dll
-    // if (v[0]-2 >=0 && v[1]-1 >=0) {
-    if (v.coords[0]-2 >=0 && v.coords[1]-1 >=0) {
-        // let move = newGraph.getVertex(v[0]-2,v[1]-1)[0]
-        // newGraph.addEdge(v, move)
-        v.nextMoves.push(v.coords[0]-2, v.coords[1]-1)
-    } 
-
-    //ddl
-    // if (v[0]-1 >=0 && v[1]-2 >=0) {
-    if (v.coords[0]-1 >=0 && v.coords[1]-2 >=0) {
-        // let move = newGraph.getVertex(v[0]-1,v[1]-2)[0]
-        // newGraph.addEdge(v, move)
-        v.nextMoves.push(v.coords[0]-1, v.coords[1]-2)
-    } 
-
-    //ddr
-    // if (v[0]+1 < 8 && v[1]-2 >=0) {
-    if (v.coords[0]+1 < 8 && v.coords[1]-2 >=0) {
-        // let move = newGraph.getVertex(v[0]+1,v[1]-2)[0]
-        // newGraph.addEdge(v, move)
-        v.nextMoves.push(v.coords[0]+1, v.coords[1]-2)
-    } 
-
-    //drr
-    // if (v[0]+2 < 8 && v[1]-1 >=0) {
-    if (v.coords[0]+2 < 8 && v.coords[1]-1 >=0) {
-        // let move = newGraph.getVertex(v[0]+2,v[1]-1)[0]
-        // newGraph.addEdge(v, move)
-        v.nextMoves.push(v.coords[0]+2, v.coords[1]-1)
-    } 
+function getNodeFromList(position, list) {
+    for (node of list) {
+        if (JSON.stringify(position) === JSON.stringify(node.coords))
+        return node;
+    }
 }
 
-// return when a certain position is reached. hmm, how do i build the counter and queue? what would it do?
-
-// start and end are arrays representing coordinates
-// add to an array for visited nodes
-// add to an array for the queue?
-
-// i'm sure i don't need a queue to go until it is length 0..., just need to stop when node is found
-// is the queue flawed?
-// how do i add the correct move first, and if it isn't there, add all of the things; i think i've done that
-// but then how do i... only add the shortest path?
-// OH SHIT is this where level order comes in? do i need to build a tree from this? how do i build a tree from these moves?
-// okay, might 
-
 function knightMoves(start, end) {
-
-    function recursiveBuildTree(position, end, visited=[start], queue=[]) {
+    // function recursiveBuildTree(position, end, visited=[start], queue=[], accessed=[start]) {
+    function recursiveBuildTree(position, end, queue=[], accessed=[start]) {
+        let node = getNodeFromList(position, vertices)
         let shortestPath = `${JSON.stringify(position)}`;
-        // get neighbouring moves of current position; newGraph stores all positions and neighbouring moves for each position
-        let moves = newGraph.getVertex(position[0],position[1])[1]
-        // first, determine if any of these moves are the end position.
-        for (v of moves) {
-            // check if current move is the end position
-            if (JSON.stringify(v) === JSON.stringify(end)) {
-                visited.push(end)
-                shortestPath += ` ${JSON.stringify(v)}`
-                console.log(shortestPath)
+        accessed.push(position)
 
+        // check if current move leads to the end position
+        for (v of node.neighbouringMoves) {
+            if (JSON.stringify(v) === JSON.stringify(end)) {
+                // do some stuff required to check if path can be added
+                // i'm not sure i even need the visited array
+                // visited.push(end)
+                // i'm not convinced that this does what i expect... these don't have that property. i need to get the node
+                // so here, i'm making the chain true
+                node.partOfChain = true;
+                // start building the chain
+                shortestPath += ` ${JSON.stringify(v)}`
                 return shortestPath;
             }
         }
 
-        // else, add all moves (not already made) to queue
-        for (v of moves) {
+        // else, add all moves (not already made) to queue and visited 
+        for (v of node.neighbouringMoves) {
             // test if the move has been made already (in visited array)
-            if (!isArrayInArray(visited, v)) {
-                visited.push(v)
+            // if (!isArrayInArray(visited, v)) {
+            //     visited.push(v)
                 queue.push(v)
-            }
+            // }
         }
 
         // get the position at the start of the queue
-        let nextVertex = queue[0];
+        let nextNeighbour = queue[0];
         queue.shift()
-        // call again
-         let recursivePath = recursiveBuildTree(nextVertex, end, visited, queue)
-         // if path eventually contains the end position, add to the chain
-         if (recursivePath.includes(JSON.stringify(end))) {
-            shortestPath += recursivePath
-         }
-        // return shortestPath;
-        return '';
-    }
+        // store reference to future return values
+        let recursivePath = '';
+        //recursive path will first return the end position and the previous node. it will also change the previous node
+        //  recursivePath += recursiveBuildTree(nextNeighbour, end, visited, queue, accessed)
+         recursivePath += recursiveBuildTree(nextNeighbour, end, queue, accessed)
 
+         // this is the end game
+
+         // now, recursive path is used to add to chain
+        if (recursivePath.includes(JSON.stringify(end))) {
+            // check the neighbouring moves once more
+            for (v of node.neighbouringMoves) {
+                
+                let checkNodeIsPartOfChain = getNodeFromList(v,vertices)
+
+                // this should find the previous value; what do i want to do with it? 
+                // if (isArrayInArray(visited, v) && v.partOfChain === true) {
+                
+                // maybe i need to get the nodes.... like node = getNodeFromList(v)
+                // if 
+
+                //does this still hold?
+                if (isArrayInArray(accessed, v)
+                 && checkNodeIsPartOfChain.partOfChain === true
+                )
+                 {
+                    node.partOfChain = true;
+                    recursivePath = JSON.stringify(position) + recursivePath;
+                    return recursivePath;
+                }
+            }
+            return '' + recursivePath;
+        }
+    }
     return recursiveBuildTree(start, end)
 }
 
 console.log(
-knightMoves([3,3], [0,0])
-// knightMoves([0,0], [3,3])
+// knightMoves([3,3], [0,0])
+// knightMoves([4,3], [0,0])
+knightMoves([0,0], [3,3])
 )
